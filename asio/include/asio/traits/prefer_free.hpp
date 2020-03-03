dade92737d5710e4655801203a029b83a26cae06
Christopher Kohlhoff <chris@kohlhoff.com>
2020-03-03 22:33:56 +1100

    Add properties implementation.

---
+++ asio/include/asio/traits/prefer_free.hpp
@@ -0,0 +1,214 @@
+//
+// traits/prefer_free.hpp
+// ~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2020 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef ASIO_TRAITS_PREFER_FREE_HPP
+#define ASIO_TRAITS_PREFER_FREE_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include "asio/detail/config.hpp"
+#include "asio/detail/type_traits.hpp"
+
+namespace asio {
+namespace traits {
+
+template <typename T, typename Property, typename = void>
+struct prefer_free_default;
+
+template <typename T, typename Property, typename = void>
+struct prefer_free;
+
+} // namespace traits
+namespace detail {
+
+#if defined(ASIO_HAS_DECLTYPE) \
+  && defined(ASIO_HAS_NOEXCEPT)
+
+#if defined(ASIO_HAS_WORKING_EXPRESSION_SFINAE)
+
+template <typename T, typename Property, typename = void>
+struct prefer_free_trait
+{
+  ASIO_STATIC_CONSTEXPR(bool, is_valid = false);
+  ASIO_STATIC_CONSTEXPR(bool, is_noexcept = false);
+};
+
+template <typename T, typename Property>
+struct prefer_free_trait<T, Property,
+  typename void_type<
+    decltype(prefer(declval<T>(), declval<Property>()))
+  >::type>
+{
+  ASIO_STATIC_CONSTEXPR(bool, is_valid = true);
+
+  using result_type = decltype(
+    prefer(declval<T>(), declval<Property>()));
+
+  ASIO_STATIC_CONSTEXPR(bool, is_noexcept = noexcept(
+    prefer(declval<T>(), declval<Property>())));
+};
+
+#else // defined(ASIO_HAS_WORKING_EXPRESSION_SFINAE)
+
+template <typename T, typename Property,
+  typename = decltype(prefer(declval<T>(), declval<Property>()))>
+struct prefer_free_trait
+{
+  ASIO_STATIC_CONSTEXPR(bool, is_valid = true);
+
+  using result_type = decltype(
+    prefer(declval<T>(), declval<Property>()));
+
+  ASIO_STATIC_CONSTEXPR(bool, is_noexcept = noexcept(
+    prefer(declval<T>(), declval<Property>())));
+};
+
+struct no_prefer {};
+
+template <typename T, typename Property>
+no_prefer prefer(T, Property);
+
+template <typename T, typename Property>
+struct prefer_free_trait<T, Property, no_prefer>
+{
+  ASIO_STATIC_CONSTEXPR(bool, is_valid = false);
+  ASIO_STATIC_CONSTEXPR(bool, is_noexcept = false);
+};
+
+#endif // defined(ASIO_HAS_WORKING_EXPRESSION_SFINAE)
+
+#else // defined(ASIO_HAS_DECLTYPE)
+      //   && defined(ASIO_HAS_NOEXCEPT)
+
+template <typename T, typename Property>
+struct prefer_free_trait
+{
+  ASIO_STATIC_CONSTEXPR(bool, is_valid = false);
+  ASIO_STATIC_CONSTEXPR(bool, is_noexcept = false);
+};
+
+template <typename T, typename Property>
+struct prefer_free_trait<T, const Property> :
+  traits::prefer_free<T, Property>
+{
+};
+
+template <typename T, typename Property>
+struct prefer_free_trait<T, const Property&> :
+  traits::prefer_free<T, Property>
+{
+};
+
+template <typename T, typename Property>
+struct prefer_free_trait<const T, Property> :
+  traits::prefer_free<T, Property>
+{
+};
+
+template <typename T, typename Property>
+struct prefer_free_trait<const T, const Property> :
+  traits::prefer_free<T, Property>
+{
+};
+
+template <typename T, typename Property>
+struct prefer_free_trait<const T, const Property&> :
+  traits::prefer_free<T, Property>
+{
+};
+
+template <typename T, typename Property>
+struct prefer_free_trait<const T&, Property> :
+  traits::prefer_free<T, Property>
+{
+};
+
+template <typename T, typename Property>
+struct prefer_free_trait<const T&, const Property> :
+  traits::prefer_free<T, Property>
+{
+};
+
+template <typename T, typename Property>
+struct prefer_free_trait<const T&, const Property&> :
+  traits::prefer_free<T, Property>
+{
+};
+
+#if defined(ASIO_HAS_MOVE)
+
+template <typename T, typename Property>
+struct prefer_free_trait<T&&, Property> :
+  traits::prefer_free<T, Property>
+{
+};
+
+template <typename T, typename Property>
+struct prefer_free_trait<T, Property&&> :
+  traits::prefer_free<T, Property>
+{
+};
+
+template <typename T, typename Property>
+struct prefer_free_trait<T&&, const Property> :
+  traits::prefer_free<T, Property>
+{
+};
+
+template <typename T, typename Property>
+struct prefer_free_trait<const T, Property&&> :
+  traits::prefer_free<T, Property>
+{
+};
+
+template <typename T, typename Property>
+struct prefer_free_trait<T&&, const Property&> :
+  traits::prefer_free<T, Property>
+{
+};
+
+template <typename T, typename Property>
+struct prefer_free_trait<const T&, Property&&> :
+  traits::prefer_free<T, Property>
+{
+};
+
+template <typename T, typename Property>
+struct prefer_free_trait<T&&, Property&&> :
+  traits::prefer_free<T, Property>
+{
+};
+
+#endif //  defined(ASIO_HAS_MOVE)
+
+#endif // defined(ASIO_HAS_DECLTYPE)
+       //   && defined(ASIO_HAS_NOEXCEPT)
+
+} // namespace detail
+namespace traits {
+
+template <typename T, typename Property, typename>
+struct prefer_free_default :
+  detail::prefer_free_trait<T, Property>
+{
+};
+
+template <typename T, typename Property, typename>
+struct prefer_free :
+  prefer_free_default<T, Property>
+{
+};
+
+} // namespace traits
+} // namespace asio
+
+#endif // ASIO_TRAITS_PREFER_FREE_HPP
